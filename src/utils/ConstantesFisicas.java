05b99c4e728bd200e3583106cfe8f67b987f4b20
ronnyrufino1 <ronny.rufino1@gmail.com>
2025-11-19 14:35:10 -0300

    Create ConstantesFisicas.java

---
+++ src/utils/ConstantesFisicas.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class ConstantesFisicas {
+    public static final double VELOCIDADE_DA_LUZ = 299792458.0; // m/s
+    public static final double INDICE_AR = 1.0;
+    public static final double INDICE_AGUA = 1.33;
+    public static final double INDICE_VIDRO = 1.5;
+}
