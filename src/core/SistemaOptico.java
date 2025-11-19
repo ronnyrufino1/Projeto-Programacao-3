9ba147a88277612d460436f1d82f458c68d3e3f5
ronnyrufino1 <ronny.rufino1@gmail.com>
2025-11-19 14:34:39 -0300

    Update SistemaOptico.java

--- src/core/SistemaOptico.java
+++ src/core/SistemaOptico.java
@@ -1,4 +1,39 @@
 package core;
 
+import strategy.Absorcao;
+import strategy.Reflexao;
+import strategy.Refracao;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SistemaOptico {
+    private List<EstruturaOpticaAbstract> components = new ArrayList<>();
+
+    public void add(Reflexao component) {
+        components.add(component);
+    }
+
+    //@Override
+    public void processo(FeixeLuz feixeLuz) {
+        for (EstruturaOpticaAbstract component : components) {
+            component.processo(feixeLuz);
+        }
+    }
+
+    //@Override
+    public String getTipo() {
+        return "Sistema Óptico";
+    }
+
+    //@Override
+    public String getNome() {
+        return "Sistema Completo";
+    }
+
+    public void add(Refracao refracao) {
+    }
+
+    public void add(Absorcao absorcao) {
+    }
 }
