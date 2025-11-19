a58419d1a116dba95cd3b06df7d78db6ea45879c
ronnyrufino1 <ronny.rufino1@gmail.com>
2025-11-19 14:34:41 -0300

    Update FeixeLuzFactory.java

--- src/factory/FeixeLuzFactory.java
+++ src/factory/FeixeLuzFactory.java
@@ -1,4 +1,18 @@
 package factory;
 
+import core.FeixeLuz;
+
 public class FeixeLuzFactory {
+    public static FeixeLuz createBeam(String type, double intensidade, double comprimentoDeOnda, String polaridade, double angulo) {
+        switch (type.toLowerCase()) {
+            case "laser":
+                return new FeixeLuz(intensidade, comprimentoDeOnda, polaridade, angulo);
+            case "led":
+                return new FeixeLuz(intensidade, comprimentoDeOnda, polaridade, angulo);
+            case "solar":
+                return new FeixeLuz(intensidade, comprimentoDeOnda, polaridade, angulo);
+            default:
+                throw new IllegalArgumentException("Tipo de feixe desconhecido: " + type);
+        }
+    }
 }
